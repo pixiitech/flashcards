8e4630f4ad56992c9a7d5bf3c24255ec6f6b4ff5
Gregory Hedrick <gregory@pixiitech.net>
2016-05-17 10:33:08 -0400

    Syncing up

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -6,11 +6,12 @@
 function correctAnswer(response) {
 	keys = $(".answerkeyword");
 	for (var i = 0; i < keys.length; i++) {
+		alert("checking if " + response.toLowerCase() + " === " + keys[i].innerHTML.toLowerCase());
 		if (response.toLowerCase() === keys[i].innerHTML.toLowerCase()) {
 			return true;
 		}
-    }
-  	return false;
+  }
+  return false;
 }
 
 //Answer submit function
